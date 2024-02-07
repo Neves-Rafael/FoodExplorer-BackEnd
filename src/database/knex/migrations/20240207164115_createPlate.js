1821483582585ebbfb6c37db1e2e661e04fcbca7
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-02-07 13:55:00 -0300

    :sparkles: Feat createPlate migration

---
+++ src/database/knex/migrations/20240207164115_createPlate.js
@@ -0,0 +1,13 @@
+exports.up = (knex) =>
+  knex.schema.createPlate("plates", (table) => {
+    table.increments("id");
+    table.text("name");
+    table.text("category");
+    table.text("description");
+    table.text("value");
+    table.text("image");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("plates");
