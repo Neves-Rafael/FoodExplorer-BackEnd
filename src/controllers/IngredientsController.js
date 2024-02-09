2115dc4b735d1ed680c4d2d70d85b649bf5a3fae
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-02-09 20:33:13 -0300

    :sparkles: Feat IngredientsController

---
+++ src/controllers/IngredientsController.js
@@ -0,0 +1,9 @@
+const knex = require("../database/knex");
+class IngredientsController {
+  async index(request, response) {
+    const ingredients = await knex("ingredients");
+    return response.json(ingredients);
+  }
+}
+
+module.exports = IngredientsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const userRoutes = require("./user.routes");
 const platesRoutes = require("./plate.routes");
+const ingredientsRoutes = require("./ingredients.routes");
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/plates", platesRoutes);
+routes.use("/ingredients", ingredientsRoutes);
 
 module.exports = routes;
