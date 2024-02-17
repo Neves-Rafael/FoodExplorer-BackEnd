971ccea52cfefeb1851210af9f3f450e99dce30f
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-02-16 22:13:48 -0300

    :sparkles: Feat PlateImageController

---
+++ src/controllers/PlateImageController.js
@@ -0,0 +1,32 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const DiskStorage = require("../providers/DiskStorage");
+
+class PlateImageController {
+  async update(request, response) {
+    const { plate_id } = request.params;
+    //verificar se o usuário é admin
+    const imageFilename = request.file.filename;
+    const diskStorage = new DiskStorage();
+
+    const plate = await knex("plates").where({ id: plate_id }).first();
+    console.log(plate);
+
+    if (!plate) {
+      throw new AppError("Você não tem permissão!");
+    }
+
+    if (plate.image) {
+      await diskStorage.deleteFile(plate.image);
+    }
+
+    const filename = await diskStorage.saveFile(imageFilename);
+    plate.image = filename;
+
+    await knex("plates").update(plate).where({ id: plate_id });
+
+    return response.json(plate);
+  }
+}
+
+module.exports = PlateImageController;
--- src/routes/plate.routes.js
+++ src/routes/plate.routes.js
@@ -1,9 +1,15 @@
+const multer = require("multer");
 const { Router } = require("express");
-const PlatesController = require("../controllers/PlatesController");
+const uploadConfig = require("../configs/upload");
 const ensureAuth = require("../middlewares/ensureAuth");
+const PlatesController = require("../controllers/PlatesController");
+const PlateImageController = require("../controllers/PlateImageController");
 
 const platesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
+
 const platesController = new PlatesController();
+const plateImageController = new PlateImageController();
 
 platesRoutes.use(ensureAuth);
 
@@ -12,4 +18,6 @@ platesRoutes.get("/", platesController.index);
 platesRoutes.get("/:id", platesController.show);
 platesRoutes.put("/:id", platesController.update);
 platesRoutes.delete("/:id", platesController.delete);
+platesRoutes.patch("/image/:plate_id", ensureAuth, upload.single("image"), plateImageController.update);
+
 module.exports = platesRoutes;
