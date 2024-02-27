6e2abb184eaa5f5edd04f835f96b82135423d9a6
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-02-27 00:29:10 -0300

    :sparkles: Feat a validatedController

---
+++ src/controllers/ValidateController.js
@@ -0,0 +1,18 @@
+const knex = require("knex");
+const AppError = require("../utils/AppError");
+
+class ValidateController{
+  async execute(request, response){
+    const { user } = request;
+
+    const checkUserExist = await knex("users").where({ id: user.id });
+
+    if(checkUserExist.length === 0 ){
+      throw new AppError("Unauthorized", 401)
+    }
+
+    return response.status(200).json();
+  }
+}
+
+module.exports = ValidateController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const UserController = require("../controllers/UsersController");
 const ensureAuth = require("../middlewares/ensureAuth");
+const ValidateController = require("../../src/controllers/ValidateController")
 
 const usersRoutes = Router();
 const userController = new UserController();
+const validateController = new ValidateController();
 
+usersRoutes.get("/validated",ensureAuth, validateController.execute)
 usersRoutes.post("/", userController.create);
 usersRoutes.put("/", ensureAuth, userController.update);
 
