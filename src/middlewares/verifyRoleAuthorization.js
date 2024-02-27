c09c7e95bd5ce5795fd26e892404cbe2ae9187f9
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-02-26 21:37:58 -0300

    :sparkles: Feat apply authentication middleware to routes

--- src/middlewares/verifyRoleAuthorization.js
+++ src/middlewares/verifyRoleAuthorization.js
@@ -2,14 +2,16 @@ const AppError = require("../utils/AppError");
 
 //adicionar em rotas que não terão permissão de admin
 
-export function verifyRoleAuthorization(verifyRole) {
+function verifyRoleAuthorization(verifyRole) {
   return (request, response, next) => {
-    const { user } = request.user;
+    const { role } = request.user;
 
-    if (role !== verifyRole) {
+    if (verifyRole.includes(role)) {
+      return next();
+    } else {  
       throw new AppError("Não autorizado!", 401);
     }
-
-    return next();
   };
 }
+
+module.exports = verifyRoleAuthorization;
--- src/routes/plate.routes.js
+++ src/routes/plate.routes.js
@@ -1,9 +1,10 @@
-const multer = require("multer");
-const { Router } = require("express");
-const uploadConfig = require("../configs/upload");
-const ensureAuth = require("../middlewares/ensureAuth");
-const PlatesController = require("../controllers/PlatesController");
-const PlateImageController = require("../controllers/PlateImageController");
+const multer = require('multer');
+const { Router } = require('express');
+const uploadConfig = require('../configs/upload');
+const ensureAuth = require('../middlewares/ensureAuth');
+const PlatesController = require('../controllers/PlatesController');
+const PlateImageController = require('../controllers/PlateImageController');
+const verifyRoleAuthorization = require('../middlewares/verifyRoleAuthorization');
 
 const platesRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
@@ -13,11 +14,20 @@ const plateImageController = new PlateImageController();
 
 platesRoutes.use(ensureAuth);
 
-platesRoutes.post("/", platesController.create);
-platesRoutes.get("/", platesController.index);
-platesRoutes.get("/:id", platesController.show);
-platesRoutes.put("/:id", platesController.update);
-platesRoutes.delete("/:id", platesController.delete);
-platesRoutes.patch("/image/:plate_id", ensureAuth, upload.single("image"), plateImageController.update);
+platesRoutes.post('/',verifyRoleAuthorization('admin'), platesController.create);
+
+platesRoutes.get('/', platesController.index);
+platesRoutes.get('/:id', platesController.show);
+
+platesRoutes.put('/:id',verifyRoleAuthorization('admin'), platesController.update);
+platesRoutes.delete('/:id', verifyRoleAuthorization('admin'), platesController.delete);
+
+platesRoutes.patch(
+  '/image/:plate_id', 
+  ensureAuth, 
+  upload.single('image'),
+  verifyRoleAuthorization('admin'),
+  plateImageController.update
+  );
 
 module.exports = platesRoutes;
