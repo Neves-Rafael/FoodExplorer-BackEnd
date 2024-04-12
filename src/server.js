5ec153c703fdae0a2ca1d3c8cf85dab89901629b
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-12 14:01:32 -0300

    :bug: Fix origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://foodexplorer-delivery.netlify.app/",
+      "http://127.0.0.1:5500",
+    ],
     credentials: true,
   })
 );
