1c82adf2ae6a48c4b0728adf63059f4947c3b793
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-11 22:40:39 -0300

    :bug: Fix cors position

--- src/server.js
+++ src/server.js
@@ -8,19 +8,19 @@ const uploadConfig = require("./configs/upload");
 const cookieParser = require("cookie-parser");
 const app = express();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes);
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(
   cors({
     origin: [
-      "https://foodexplorer-delivery.netlify.app/",
+      "https://foodexplorer-delivery.netlify.app",
       "http://127.0.0.1:5173/",
     ],
     credentials: true,
   })
 );
-app.use(cookieParser());
-app.use(express.json());
-app.use(routes);
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
