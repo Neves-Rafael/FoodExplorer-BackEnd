cdedf357ea9cc0b241b14ccbbc7e8b92003f37f5
Rafael Neves <nevesrafael.dev@gmail.com>
2024-04-14 22:00:51 -0300

    :bug: Fix authorization in payment route

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-const PaymentController = require("../controllers/PaymentController");
 const ensureAuth = require("../middlewares/ensureAuth");
+const PaymentController = require("../controllers/PaymentController");
+const verifyRoleAuthorization = require("../middlewares/verifyRoleAuthorization");
 
 const paymentRoutes = Router();
 const paymentController = new PaymentController();
@@ -9,6 +10,11 @@ paymentRoutes.get("/:id", ensureAuth, paymentController.show);
 paymentRoutes.put("/qrcode/:id", paymentController.execute);
 paymentRoutes.get("/", ensureAuth, paymentController.index);
 paymentRoutes.post("/", ensureAuth, paymentController.create);
-paymentRoutes.patch("/:id", ensureAuth, paymentController.update);
+paymentRoutes.patch(
+  "/:id",
+  ensureAuth,
+  verifyRoleAuthorization("admin"),
+  paymentController.update
+);
 
 module.exports = paymentRoutes;
